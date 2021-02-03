4eec2ed5aefb2ff4a67026ba4d9e7774350b7b3c
Davide Faconti <dfaconti@seegrid.com>
2021-02-03 23:34:31 +0100

    Rebase from 'release/melodic/behaviortree_cpp_v3'

--- src/basic_types.cpp
+++ src/basic_types.cpp
@@ -120,15 +120,31 @@ unsigned convertFromString<unsigned>(StringView str)
 }
 
 template <>
+unsigned long convertFromString<unsigned long>(StringView str)
+{
+    return std::stoul(str.data());
+}
+
+template <>
 double convertFromString<double>(StringView str)
 {
     // see issue #120
     // http://quick-bench.com/DWaXRWnxtxvwIMvZy2DxVPEKJnE
 
-    const auto old_locale = setlocale(LC_NUMERIC,nullptr);
+    std::string old_locale = setlocale(LC_NUMERIC,nullptr);
     setlocale(LC_NUMERIC,"C");
     double val = std::stod(str.data());
-    setlocale(LC_NUMERIC,old_locale);
+    setlocale(LC_NUMERIC, old_locale.c_str());
+    return val;
+}
+
+template <>
+float convertFromString<float>(StringView str)
+{
+    std::string old_locale = setlocale(LC_NUMERIC,nullptr);
+    setlocale(LC_NUMERIC,"C");
+    float val = std::stof(str.data());
+    setlocale(LC_NUMERIC, old_locale.c_str());
     return val;
 }
 
